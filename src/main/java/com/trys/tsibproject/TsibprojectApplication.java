e0aeb48ce833413e74ad84264887c3212496edf7
이충재 <knm8643@nate.com>
2022-11-22 22:40:04 +0900

    스프링부트 메이븐 mysql 연동 성공

---
+++ src/main/java/com/trys/tsibproject/TsibprojectApplication.java
@@ -0,0 +1,13 @@
+package com.trys.tsibproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TsibprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TsibprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trys/tsibproject/TsibprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trys.tsibproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TsibprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
