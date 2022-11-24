96dd3d8aeae5953b06a5a4f5104ab2c3e4f79fea
이충재 <knm8643@nate.com>
2022-11-24 21:22:40 +0900

    모달팝업기초공사/ api파라미터 전송테스트

--- src/main/java/com/trys/tsibproject/controller/HomeController.java
+++ src/main/java/com/trys/tsibproject/controller/HomeController.java
@@ -2,6 +2,7 @@ package com.trys.tsibproject.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -9,5 +10,15 @@ public class HomeController {
     @RequestMapping
     public String home(Model model){
         return "home";
+    }
+
+    // home.html에서 쏘기 때문에 이쪽으로 정보 값이 던져짐
+    // 후에 이쪽에서 분기점 나누면 될것같음
+    // or 아예 form 형식이 아니라 ajax로 분기점 나눠서 던지면 될듯
+    // 11-24 작업은 여기서 마무리
+    @PostMapping("/member")
+    public String testMember2(String privateName){
+        System.out.println("테스트2");
+        return privateName;
     }
 }
--- src/main/java/com/trys/tsibproject/controller/memberInfoController.java
+++ src/main/java/com/trys/tsibproject/controller/memberInfoController.java
@@ -1,7 +1,16 @@
 package com.trys.tsibproject.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/member")
 public class memberInfoController {
+
+    @PostMapping("/test")
+    public String testMember(){
+        System.out.println("테스트");
+        return "hi";
+    }
 }
