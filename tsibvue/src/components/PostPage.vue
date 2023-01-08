1196584f0de5a573849111653776e22231bc2138
Chan <69744429+000chan@users.noreply.github.com>
2023-01-08 23:12:03 +0900

    글쓰기 페이지 맛보기

--- tsibvue/src/components/PostPage.vue
+++ tsibvue/src/components/PostPage.vue
@@ -1,13 +1,50 @@
 <template>
-  <h1>post page</h1>
+  <div class="전체구역">
+    <div id="글쓰는구역">
+      <input id="글상자" type="text" v-model="content" placeholder="글을 입력하세요.">
+    </div>
+    <div id="미리보는구역">
+      <div id="미리보기상자">{{content}}</div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: "PostPage"
+  name: "PostPage",
+  components: {},
+  data() {
+    return {
+      content: ""
+    }
+  }
 }
 </script>
 
 <style scoped>
-
+/* 임시 */
+  .전체구역 {
+    width: 500px;
+    height: 500px;
+  }
+  #글쓰는구역 {
+    border: 1px solid black;
+    width: 200px;
+    height: 200px;
+    float: left;
+  }
+  #글상자 {
+    width: 100px;
+    height: 100px;
+    margin: 50px 0 50px 0;
+  }
+  #미리보는구역 {
+    border: 1px solid black;
+    width: 200px;
+    height: 200px;
+    float: left;
+  }
+  #미리보기상자 {
+    margin: 50px 0 50px 0;
+  }
 </style>
