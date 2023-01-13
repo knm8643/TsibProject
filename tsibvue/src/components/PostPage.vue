52d403d358a5fafdfb14dd00b2566933a8cba7fd
김영찬-PC\김영찬-PC <ychan@tricycle.co.kr>
2023-01-13 15:52:09 +0900

    오늘 사무실 너무 더움

--- tsibvue/src/components/PostPage.vue
+++ tsibvue/src/components/PostPage.vue
@@ -1,39 +1,31 @@
 <template>
+  <h3 style="margin-bottom:0;">게시글 작성 페이지</h3>
   <div class="전체글구역">
     <div id="미리보기전체구역">
-      <div id="미리보기버튼구역">
-        <button>1</button>
-        <button>2</button>
-        <button>3</button>
-        <input type="file">
-      </div>
-      <div id="미리보기태그구역">
-        #태그_예시1, #태그_예시2, #태그_예시3,
-        {{ tag ? "#" + tag : "" }}
-      </div>
-      <div id="미리보기제목구역">{{ title }}</div>
-      <div id="미리보기내용구역">{{ content }}</div>
+      <div id="미리보기_이미지">[이미지 업로드 테스트 중]</div>
+      <p id="미리보기_태그">#태그_예시1, #태그_예시2, #태그_예시3, {{ tag ? "#" + tag : "" }}</p>
+      <p id="미리보기_제목">{{ title }}</p>
+      <p id="미리보기_내용">{{ content }}</p>
     </div>
     <div id="글쓰기전체구역">
-      <div id="글쓰기버튼구역">
-        <button>1</button>
-        <button>2</button>
-        <button>3</button>
-        <input type="file">
-      </div>
-      <div id="글쓰기태그지정구역">
-        <label style="color:white;">태그 할 내용을 적어주세요:&nbsp;&nbsp;#</label>
-        <input type="text" v-model="tag" placeholder="공백 없이 작성해주세요.">
-      </div>
-      <div id="글쓰기태그구역">
-        #태그_예시1,#태그_예시2, #태그_예시3
-      </div>
-      <textarea id="글쓰기제목구역" type="text" v-model="title" placeholder="제목을 입력하세요. 작성하신 내용은 좌측 미리보기로 확인 가능합니다."></textarea>
-      <textarea id="글쓰기내용구역" type="text" v-model="content" placeholder="글을 입력하세요. 작성하신 내용은 좌측 미리보기로 확인 가능합니다."></textarea>
+      <form style="height:600px; margin:0;">
+        <p id="글쓰기_버튼">
+          <button>1</button>
+          <button>2</button>
+          <button>3</button>
+          <input type="file">
+        </p>
+        <p id="글쓰기_태그">
+          <label style="font-size: 14px">#태그_예시1, #태그_예시2, #태그_예시3, #</label>
+          <input type="text" v-model="tag">
+        </p>
+        <textarea id="글쓰기_제목" type="text" v-model="title" placeholder=" 제목을 입력하세요."></textarea>
+        <textarea id="글쓰기_내용" type="text" v-model="content" placeholder="  내용을 입력하세요."></textarea>
+        <p style="margin:0; height:100px;">
+          <button id="글쓰기_저장" type="submit" @click="writePost">저장하기</button>
+        </p>
+      </form>
     </div>
-  </div>
-  <div id="저장버튼구역">
-    <button id="저장버튼" type="submit" @click="writePost">저장하기</button>
   </div>
 </template>
 
@@ -51,28 +43,26 @@ export default {
     }
   },
   methods: {
-    writePost(){
+    writePost() {
       axios.post('/post/write', {
         content: this.content
-      })
-          .then(function(response){
-            alert('저장되었습니다.');
-            console.log(response);
-          })
-          .catch(function(error){
-            alert('저장에 실패하였습니다.');
-            console.log(error)
-          });
-    }
+      }).then(function (response) {
+        alert('저장되었습니다.');
+        console.log(response);
+      }).catch(function (error) {
+        alert('저장에 실패하였습니다.');
+        console.log(error)
+      });
+    },
   }
 }
 </script>
 
 <style scoped>
-/* 전체 */
-.전체글구역{
-  width: 100%;
-  height: 400px;
+.전체글구역 {
+  width: 90%;
+  height: 700px;
+  margin: auto;
 
   /* 가로, 세로 가운데 정렬 */
   display: flex;
@@ -80,13 +70,12 @@ export default {
   align-items: center;
 }
 
-/* 미리보기 */
-#미리보기전체구역{
-  background-color: whitesmoke;
+#미리보기전체구역 {
   width: 49%;
-  height: 350px;
+  height: 600px;
   float: left;
   overflow: hidden;
+  border: 1px solid black;
 
   /* 가로, 세로 가운데 정렬 */
   display: inline-block;
@@ -94,12 +83,19 @@ export default {
   align-items: center;
 }
 
-/* 미리보기 버튼 */
-#미리보기버튼구역{
+#미리보기_이미지 {
   width: 100%;
-  height: 10%;
-  background-color: powderblue;
+  height: 150px;
+  margin: 0;
+  border-bottom: 1px solid black;
+}
 
+#미리보기_태그 {
+  width: 100%;
+  height: 50px;
+  color: black;
+  margin: 0;
+  border-bottom: 1px solid black;
 
   /* 가로, 세로 가운데 정렬 */
   display: flex;
@@ -107,142 +103,13 @@ export default {
   align-items: center;
 }
 
-/* 미리보기 태그 */
-#미리보기태그구역{
-  width: 100%;
-  height: 20%;
-  background-color: darkseagreen;
-  color: white;
-
-  /* 가로, 세로 가운데 정렬 */
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-
-/* 미리보기 제목 */
-#미리보기제목구역{
-  background-color: dimgrey;
-  width: 100%;
-  height: 20%;
-  color: white;
-  font-size: 1.4em;
-
-
-  /* 가로, 세로 가운데 정렬 */
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-
-/* 미리보기 내용 */
-#미리보기내용구역{
-  background-color: grey;
-  width: 100%;
-  height: 50%;
-  color: white;
-  font-size: 1.1em;
-
-
-  /* 가로, 세로 가운데 정렬 */
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-
-/* 글쓰기 */
-#글쓰기전체구역{
-  width: 49%;
-  height: 350px;
-  float: left;
-  overflow: hidden;
-
-  /* 가로, 세로 가운데 정렬 */
-  display: inline-block;
-  justify-content: center;
-  align-items: center;
-}
-
-/* 글쓰기 버튼 */
-#글쓰기버튼구역{
-  width: 100%;
-  height: 10%;
-  background-color: #42b983;
-
-
-  /* 가로, 세로 가운데 정렬 */
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-
-/* 글쓰기 태그 (작성하는 태그) */
-#글쓰기태그지정구역{
-  width: 100%;
-  height: 10%;
-  background-color: darkslategrey;
-
-  /* 가로, 세로 가운데 정렬 */
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-
-/* 글쓰기 태그 (작성된 태그) */
-#글쓰기태그구역{
-  width: 100%;
-  height: 10%;
-  background-color: olive;
-  color: white;
-
-  /* 가로, 세로 가운데 정렬 */
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-
-/* 글쓰기 제목 */
-#글쓰기제목구역{
-  width: 100%;
-  height: 20%;
-  resize: none;
-  padding: 0%;
-  font-size: 1.4em;
-  display: block;
-  background-color: antiquewhite;
-
-  /* textarea 데코레이션 제거 */
-  border: none;
-  overflow: auto;
-  outline: none;
-  -webkit-box-shadow: none;
-  -moz-box-shadow: none;
-  box-shadow: none;
-}
-
-/* 글쓰기 내용 */
-#글쓰기내용구역{
-  width: 100%;
-  height: 560%;
-  resize: none;
-  padding: 0%;
-  font-size: 1.4em;
-  display: block;
-  background-color: navajowhite;
-
-  /* textarea 데코레이션 제거 */
-  border: none;
-  overflow: auto;
-  outline: none;
-  -webkit-box-shadow: none;
-  -moz-box-shadow: none;
-  box-shadow: none;
-}
-
-/* 저장버튼 구역 */
-#저장버튼구역{
+#미리보기_제목 {
   width: 100%;
   height: 100px;
+  color: black;
+  font-size: 2em;
+  margin: 0;
+  border-bottom: 1px solid black;
 
   /* 가로, 세로 가운데 정렬 */
   display: flex;
@@ -250,10 +117,115 @@ export default {
   align-items: center;
 }
 
-/* 저장버튼 */
-#저장버튼{
+#미리보기_내용 {
+  width: 100%;
+  height: 300px;
+  color: black;
+  font-size: 1.1em;
+  margin: 0;
+
+  /* 가로, 세로 가운데 정렬 */
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+#글쓰기전체구역 {
+  width: 49%;
+  height: 600px;
+  float: left;
+  overflow: hidden;
+  border: 1px solid black;
+  border-left: none;
+
+  /* 가로, 세로 가운데 정렬 */
+  display: inline-block;
+  justify-content: center;
+  align-items: center;
+}
+
+#글쓰기_버튼 {
+  width: 100%;
+  height: 50px;
+  resize: none;
+  padding: 0%;
+  font-size: 2em;
+  display: block;
+  margin: 0;
+
+  /* textarea 데코레이션 제거 */
+  border: 1px solid black;
+  border-left: none;
+  border-top: none;
+  overflow: auto;
+  outline: none;
+  -webkit-box-shadow: none;
+  -moz-box-shadow: none;
+  box-shadow: none;
+}
+
+#글쓰기_태그 {
+  width: 100%;
+  height: 50px;
+  resize: none;
+  padding: 0%;
+  font-size: 2em;
+  display: block;
+  margin: 0;
+
+  /* textarea 데코레이션 제거 */
+  border: 1px solid black;
+  border-left: none;
+  border-top: none;
+  border-bottom: none;
+  overflow: auto;
+  outline: none;
+  -webkit-box-shadow: none;
+  -moz-box-shadow: none;
+  box-shadow: none;
+}
+
+#글쓰기_제목 {
+  width: 100%;
+  height: 100px;
+  resize: none;
+  padding: 0%;
+  font-size: 2em;
+  display: block;
+
+  /* textarea 데코레이션 제거 */
+  border: 1px solid black;
+  border-left: none;
+  overflow: auto;
+  outline: none;
+  -webkit-box-shadow: none;
+  -moz-box-shadow: none;
+  box-shadow: none;
+}
+
+#글쓰기_내용 {
+  width: 100%;
+  height: 300px;
+  resize: none;
+  padding: 0%;
+  font-size: 1.4em;
+  display: block;
+
+  /* textarea 데코레이션 제거 */
+  border: 1px solid black;
+  border-left: none;
+  border-top: none;
+  overflow: auto;
+  outline: none;
+  -webkit-box-shadow: none;
+  -moz-box-shadow: none;
+  box-shadow: none;
+}
+
+#글쓰기_저장 {
   width: 100px;
-  height: 40px;
+  height: 50px;
   border: 1px solid dimgrey;
+  margin-top: 30px;
 }
 </style>
