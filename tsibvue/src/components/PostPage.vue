820335c14b93225f14da90deadbd961a325b6305
Chan <69744429+000chan@users.noreply.github.com>
2023-02-24 18:56:25 +0900

    글쓰기 페이지 부트스트랩 사용 테스트

--- tsibvue/src/components/PostPage.vue
+++ tsibvue/src/components/PostPage.vue
@@ -1,27 +1,31 @@
 <template>
-  <div class="row">
-    <div class="col-6" style="background-color:#e5c7ca">
+  <div class="row row-cols-2">
+    <div class="col" style="background-color:#e5c7ca; min-height: 150px;">
       미리보기 구역
       <div class="col-10" id="preview-title" style="background-color:#ece1be; margin:auto;">{{ title }}</div>
       <div class="col-10" id="preview-tag" style="background-color: beige; margin:auto;">{{ media }}</div>
       <div class="col-10" id="preview-tag" style="background-color:lightyellow; margin:auto;">{{ tag }}</div>
       <div class="col-10" id="preview-content" style="background-color:lemonchiffon; margin:auto;">{{ content }}</div>
     </div>
-    <div class="col-6" style="background-color:#badce3;">
+    <div class="col" style="background-color:#badce3; padding:0">
       글쓰기 구역
       <form v-on:submit="createPost">
-        <p>
-          <input type="text" v-model="title" placeholder="제목을 입력하세요.">
-        </p>
-        <p>
-          <input type="text" v-model="media" placeholder="사진을 입력하세요.">
-        </p>
-        <p>
-          <input type="text" v-model="tag" placeholder="태그를 입력하세요.">
-        </p>
-        <p>
-          <input type="text" v-model="content" placeholder="내용을 입력하세요.">
-        </p>
+        <div class="form-floating mb-3">
+          <input type="text" class="form-control" id="floatingInputDisabled" placeholder="" v-model="title">
+          <label for="floatingInputDisabled">제목</label>
+        </div>
+        <div class="form-floating mb-3">
+          <input type="text" class="form-control" id="floatingInputDisabled" placeholder="" v-model="media">
+          <label for="floatingInputDisabled">사진</label>
+        </div>
+        <div class="form-floating mb-3">
+          <input type="text" class="form-control" id="floatingInputDisabled" placeholder="" v-model="tag">
+          <label for="floatingInputDisabled">태그</label>
+        </div>
+        <div class="form-floating mb-3">
+          <input type="text" class="form-control" id="floatingInputDisabled" placeholder="" v-model="content">
+          <label for="floatingInputDisabled">내용</label>
+        </div>
         <button type="submit">저장하기</button>
       </form>
     </div>
