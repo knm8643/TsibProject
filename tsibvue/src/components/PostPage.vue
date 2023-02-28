c41d45b896db6c641c14ddfbfe2bd273fc18738e
김영찬-PC\김영찬-PC <ychan@tricycle.co.kr>
2023-02-28 17:52:04 +0900

    글쓰기 페이지 임시 버튼 추가

--- tsibvue/src/components/PostPage.vue
+++ tsibvue/src/components/PostPage.vue
@@ -1,32 +1,34 @@
 <template>
   <div class="row row-cols-2">
-    <div class="col" style="background-color:#e5c7ca; min-height: 150px;">
-      미리보기 구역
+    <div class="col" style="background-color:ghostwhite; min-height: 150px; padding: 0">
+      <p style="margin: 2% 0 2% 0;">(임시) 미리보기 구역</p>
       <div class="col-10" id="preview-title" style="background-color:#ece1be; margin:auto;">{{ title }}</div>
-      <div class="col-10" id="preview-tag" style="background-color: beige; margin:auto;">{{ media }}</div>
+      <div class="col-10" id="preview-media" style="background-color: beige; margin:auto;">{{ media }}</div>
       <div class="col-10" id="preview-tag" style="background-color:lightyellow; margin:auto;">{{ tag }}</div>
       <div class="col-10" id="preview-content" style="background-color:lemonchiffon; margin:auto;">{{ content }}</div>
     </div>
-    <div class="col" style="background-color:#badce3; padding:0">
-      글쓰기 구역
-      <form v-on:submit="createPost">
+    <div class="col" style="background-color:whitesmoke; padding:0">
+      <p style="margin: 2% 0 2% 0;">(임시) 글쓰기 구역</p>
+      <form v-on:submit="createPost" style="margin: 0 10% 0 10%">
         <div class="form-floating mb-3">
-          <input type="text" class="form-control" id="floatingInputDisabled" placeholder="" v-model="title">
-          <label for="floatingInputDisabled">제목</label>
+          <input type="text" class="form-control" placeholder="" v-model="title">
+          <label>제목</label>
         </div>
         <div class="form-floating mb-3">
-          <input type="text" class="form-control" id="floatingInputDisabled" placeholder="" v-model="media">
-          <label for="floatingInputDisabled">사진</label>
+          <input type="text" class="form-control" placeholder="" v-model="media">
+          <label>사진</label>
         </div>
         <div class="form-floating mb-3">
-          <input type="text" class="form-control" id="floatingInputDisabled" placeholder="" v-model="tag">
-          <label for="floatingInputDisabled">태그</label>
+          <input type="text" class="form-control" placeholder="" v-model="tag">
+          <label>태그</label>
         </div>
         <div class="form-floating mb-3">
-          <input type="text" class="form-control" id="floatingInputDisabled" placeholder="" v-model="content">
-          <label for="floatingInputDisabled">내용</label>
+          <input type="text" class="form-control" placeholder="" v-model="content">
+          <label>내용</label>
         </div>
-        <button type="submit">저장하기</button>
+        <button type="button" v-on:click="goBack" class="btn btn-outline-primary" id="goBackButton">나가기</button>
+        <button type="button" v-on:click="savePost" class="btn btn-outline-primary" id="saveButton">저장하기</button>
+        <button type="button" v-on:click="savePostTmp" class="btn btn-outline-secondary" id="tmpSaveButton">임시저장하기</button>
       </form>
     </div>
   </div>
@@ -38,4 +40,18 @@ export default postPage
 </script>
 
 <style scoped>
+.form-control {
+  width: 100%;
+  margin: 0;
+}
+#goBackButton {
+  float:left;
+  margin: 1% 0 3% 1%;
+}
+#saveButton {
+  float:right; margin: 1% 3% 3% 0;
+}
+#tmpSaveButton {
+  float:right; margin: 1% 1% 3% 0;
+}
 </style>
--- tsibvue/src/js/postPage.js
+++ tsibvue/src/js/postPage.js
@@ -6,24 +6,103 @@ export default {
             title: "",
             media: "",
             tag: "",
-            content: "",
+            content: ""
         }
     },
     methods: {
-        createPost() {
-            var url = '/board/create';
-            var data = {
-                title: this.title,
-                media: this.media,
-                tag: this.tag,
-                content: this.content
+        // 나가기 버튼
+        goBack: function (event) {
+            if (event) {
+                window.history.back();
             }
-            this.$axios.post(url, data)
-                .then(function (response) {
-                    console.log(response.data)
-                }).catch(function (error) {
-                console.log(error)
-            });
         },
+
+        // 저장 버튼
+        savePost: function (event) {
+            if (event) {
+                // 게시물 데이터
+                let postData = {
+                    title: this.title,
+                    media: this.media,
+                    tag: this.tag,
+                    content: this.content
+                }
+
+                // (임시)
+                let msg = "title: " + this.title + "\n" +
+                    "media: " + this.media + "\n" +
+                    "tag: " + this.tag + "\n" +
+                    "content: " + this.content + "\n";
+
+                // AXIOS
+                this.$axios({
+                    url: "/board/save",
+                    method: "post",
+                    params: postData
+                })
+                    // 성공
+                    .then(function (response) {
+                        alert("[ 저장하기 ]\n" +
+                            "* 데이터\n" + msg + "\n" +
+                            "* 메시지\n" + "저장에 성공했습니다." + "\n\n" +
+                            "* 응답 메시지\n" + response.data
+                        );
+                    })
+                    // 실패
+                    .catch(function (error) {
+                        if (error.response) {
+                            alert("[ 저장하기 ]\n" +
+                                "* 데이터\n" + msg + "\n" +
+                                "* 메시지\n" + "저장에 실패하였습니다." + "\n\n" +
+                                "* axios 에러 메시지\n" + error
+                            );
+                        }
+                    });
+            }
+        },
+
+        // 임시저장 버튼
+        savePostTmp: function (event) {
+            if (event) {
+                // 게시물 데이터
+                let postData = {
+                    title: this.title,
+                    media: this.media,
+                    tag: this.tag,
+                    content: this.content
+                }
+
+                // (임시)
+                let msg = "title: " + this.title + "\n" +
+                    "media: " + this.media + "\n" +
+                    "tag: " + this.tag + "\n" +
+                    "content: " + this.content + "\n";
+
+                // AXIOS
+                this.$axios({
+                    url: "/board/tmpsave",
+                    method: "post",
+                    params: postData
+                })
+                    // 성공
+                    .then(function (response) {
+                        alert("[ 임시저장하기 ]\n" +
+                            "* 데이터\n" + msg + "\n" +
+                            "* 메시지\n" + "임시저장에 성공했습니다." + "\n\n" +
+                            "* 응답 메시지\n" + response.data
+                        );
+                    })
+                    // 실패
+                    .catch(function (error) {
+                        if (error.response) {
+                            alert("[ 임시저장하기 ]\n" +
+                                "* 데이터\n" + msg + "\n" +
+                                "* 메시지\n" + "임시저장에 실패하였습니다." + "\n\n" +
+                                "* axios 에러 메시지\n" + error
+                            );
+                        }
+                    });
+            }
+        }
     }
 }
