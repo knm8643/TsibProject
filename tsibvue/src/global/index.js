b6cd510124da8cec485a1f369ba8bd8bd54342bb
이충재 <knm8643@nate.com>
2023-03-03 15:55:29 +0900

    기본basic Url 생성

--- tsibvue/src/global/index.js
+++ tsibvue/src/global/index.js
@@ -1,6 +1,9 @@
 import axios from 'axios'
 export default {
     install (vue) {
+        axios.defaults.baseURL = process.env.VUE_APP_APIX_URL
+        axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded'
+
         vue.config.globalProperties.$axios = axios
     }
 }
