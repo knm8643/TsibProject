92e7a312e46fa5f714e70ed0d64e9387099777a2
yu-rim95 <80299209+yu-rim95@users.noreply.github.com>
2023-03-07 22:12:08 +0900

    오승훈

--- tsibvue/src/components/LoginPage.vue
+++ tsibvue/src/components/LoginPage.vue
@@ -1,7 +1,7 @@
 <template>
-  <section>
-    <div class="왼쪽">
-      <p>저희에게 당싱을 알려주세요!</p>
+  <section class="login-M">
+    <div class="w-login">
+      <p>저희에게 당신을 알려주세요!</p>
     </div>
     <div class="오른쪽">
       <h2>블로그 만드는 블로그 </h2>
@@ -29,4 +29,14 @@ import loginPage from "@/js/loginPage.js";
 export default loginPage
 </script>
 
-<style scoped></style>
+<style scoped>
+.login-M{    
+    width: 745px;
+    height: 590px;
+    margin: 0 auto;
+    display: flex;
+    background: #ebebeb;
+    justify-content: space-around;
+    box-shadow: 0px 4px 4px rgba(0,0,0,.25);
+    }
+</style>
