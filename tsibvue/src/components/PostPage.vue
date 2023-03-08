171c0daba0bcb95642569afbfd8cceb489555cae
김영찬-PC\김영찬-PC <ychan@tricycle.co.kr>
2023-03-08 17:38:06 +0900

    글쓰기 페이지 나가기 버튼 위치 변경

--- tsibvue/src/components/PostPage.vue
+++ tsibvue/src/components/PostPage.vue
@@ -1,4 +1,7 @@
 <template>
+  <div class="goBackButtonDiv">
+    <button type="button" v-on:click="goBack" class="btn btn-outline-primary" id="goBackButton">나가기</button>
+  </div>
   <div class="row row-cols-2">
     <div class="col" style="background-color:ghostwhite; min-height: 150px; padding: 0">
       <p style="margin: 2% 0 2% 0;">(임시) 미리보기 구역</p>
@@ -26,7 +29,6 @@
           <input type="text" class="form-control" placeholder="" v-model="content">
           <label>내용</label>
         </div>
-        <button type="button" v-on:click="goBack" class="btn btn-outline-primary" id="goBackButton">나가기</button>
         <button type="button" v-on:click="savePostTmp" class="btn btn-outline-secondary" id="tmpSaveButton">임시저장하기</button>
         <button type="button" v-on:click="savePost" class="btn btn-outline-primary" id="saveButton">저장하기</button>
       </form>
